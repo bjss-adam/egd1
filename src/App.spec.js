da6a41336649b63d71501694e2c8cc15a1e3797b
Adam McElroy <adam.mcelroy@bjss.com>
2019-12-12 10:51:34 +0000

    Initial commit

---
+++ src/App.spec.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import App from './App';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('App', () => {
+});
