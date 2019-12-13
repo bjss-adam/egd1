d7f43915b30fd9e67d6e359bf708e069148ef7b7
Adam McElroy <adam.mcelroy@bjss.com>
2019-12-13 09:30:47 +0000

    Further amendments

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Enzyme 101
-        </h1>
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            Enzyme 101
+          </h1>
 
-        <p>
-          Let's learn about Enyzme.
-        </p>
-        <a
-          className="link"
-          href="https://airbnb.io/enzyme/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Enzyme
-        </a>
-      </header>
-    </div>
-  );
+          <p>
+            Let's learn about Enyzme.
+          </p>
+          <a
+            className="link"
+            href="https://airbnb.io/enzyme/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Enzyme
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
